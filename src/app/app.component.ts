81de58009da32cbb02efd0ab0bc71957c2ab9882
ggomyong <63125569+ggomyong@users.noreply.github.com>
2020-12-17 22:45:01 -0500

    week6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Two from '../assets/two.min.js';
+import { Sprite, SpriteService } from './services/sprite.service.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  direction:string;
+  
+  x: number=200;
+  y: number=200;
+
+  constructor(private _spriteService: SpriteService) {}
+
+  @HostListener('document:keydown', ['$event'])
+  handleKey(event: KeyboardEvent) {
+    if (event.key=='ArrowRight') {
+      this.x=this.x+10;
+      this._spriteService.sprites[0].direction='right'
+    }
+    else if (event.key=='ArrowLeft') {
+      this.x=this.x-10;
+      this._spriteService.sprites[0].direction='left';
+    }
+    else if (event.key =='ArrowUp') {
+      this.y=this.y-10;
+    }
+    else if (event.key=='ArrowDown') {
+      this.y=this.y+10;
+    }
+  }
+
+  ngOnInit(): void {
+    let elem = document.getElementById('draw-shapes');
+    let params = {fullscreen: true};
+    let two = new Two(params).appendTo(elem);
+    //loop through service
+
+    for (let i=0; i<this._spriteService.sprites.length; i++) {
+      let sprite=this._spriteService.sprites[i];
+      this._spriteService.sprites[i].sprite=two.makeSprite(sprite.url, sprite.x, sprite.y, sprite.columns, sprite.rows, sprite.fps);
+      this._spriteService.sprites[i].sprite.play(this._spriteService.sprites[i].rightFrames[0], this._spriteService.sprites[i].rightFrames[1]);
+      this._spriteService.sprites[i].sprite.scale=this._spriteService.sprites[i].scale;
+    }
+    //rectangle.scale=.7;
+    two.bind('update', (framesPerSecond)=>{
+      // this is where animatoin happens
+      this._spriteService.sprites[0].sprite.translation.x=this.x;
+        this._spriteService.sprites[0].sprite.translation.y=this.y;
+        for (let i=0; i<this._spriteService.sprites.length; i++) {
+          if (this._spriteService.sprites[i].direction != this._spriteService.sprites[i].lastDirection) {
+            this._spriteService.sprites[i].lastDirection=this._spriteService.sprites[i].direction;
+            if (this._spriteService.sprites[i].direction=='right') {
+              this._spriteService.sprites[i].sprite.play(this._spriteService.sprites[i].rightFrames[0], this._spriteService.sprites[i].rightFrames[1])
+            }
+            else {
+              this._spriteService.sprites[i].sprite.play(this._spriteService.sprites[i].leftFrames[0], this._spriteService.sprites[i].leftFrames[1])
+            }
+          }
+        }
+        
+    }).play();
+  }
+
   title = 'BabyShark';
+
 }
---
+++ src/app/services/sprite.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+
+export interface Sprite {
+  name: string;
+  visibility: boolean;
+  state: number; // 1=moving, 0=stationary
+  direction: string; // left, right, up, down.
+  lastDirection: string;
+  maxSpeed: number; 
+  acceleration: number;
+  scale: number;
+  playable: boolean;
+
+  url: string;
+  fps: number; //frames per second
+  x: number; // horizontal coordinate
+  y: number; //vertical coordinate
+
+  rows: number; // how many rows in your sprite sheet
+  columns: number; //how many columns in your sprite sheet
+
+  sprite: any;
+
+  leftFrames: number[];
+  rightFrames: number[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpriteService {
+  sprites:Sprite[]=[
+    {
+      name: 'Baby Shark',
+      visibility: true,
+      state: 0,
+      direction: 'right',
+      lastDirection: 'right',
+      maxSpeed: 10,
+      acceleration: 1,
+      scale: .7,
+      playable: true,
+      url: '../assets/sprites/babysharks.png',
+      fps: 7,
+      x: 200,
+      y: 200,
+      rows: 2,
+      columns: 2,
+      sprite: null,
+      leftFrames: [2, 3],
+      rightFrames: [0, 1]
+    },
+  ];
+
+  constructor() { }
+}
